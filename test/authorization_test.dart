74b71e54c34d5a939f3c01d20cb346b313307a7c
petersokhn015 <79665518+petersokhn015@users.noreply.github.com>
2022-11-24 12:27:38 +0200

    fixed login and register unit tests

--- test/authorization_test.dart
+++ test/authorization_test.dart
@@ -1,38 +1,41 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
+import 'package:qa/models/User.dart';
 
-import 'package:qa/main.dart';
 import 'package:qa/services/LoginService.dart';
 import 'package:qa/services/SignUpService.dart';
-import 'package:qa/utils/settings_prefs.dart';
-import 'package:qa/views/screens/login.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-
   group('Login', () {
     test('User logged in successfully', () async {
-      bool isLoggedIn = await Login("l@dev.com", "password");
-      expect(isLoggedIn, true);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      User loggedInUser = await Login("petertest@gmail.com", "petertestpass");
+      User expectedUser = User(
+          email: "petertest@gmail.com",
+          name: "petertest",
+          phoneNumber: "",
+          id: 2);
+      expect(loggedInUser, expectedUser);
     });
 
     test('User logged in unsuccessfully', () async {
-      bool isLoggedIn = await Login("whateverwhat@gmail.com", "peter11pass");
-      expect(isLoggedIn, false);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await Login("whatever@whatever.com", "password12whatever");
+      bool? loggedInUser = prefs.getBool("isLoggedin");
+      expect(loggedInUser, null);
     });
   });
 
   group('Register', () {
     test('User registered successfully', () async {
-      bool isRegistered =
-         await Register("peter11@gmail.com", "peter11pass", "peter11");
+      bool isRegistered = await Register(
+          "petertest22@gmail.com", "petertest22pass", "petertest22");
       expect(isRegistered, true);
     });
 
     test('User registered unsuccessfully', () async {
-      bool isRegistered =
-         await Register("peter", "peter11pass", "peter11");
+      bool isRegistered = await Register(
+          "petertest22@gmail.com", "petertest22pass", "petertest22");
       expect(isRegistered, false);
     });
   });
